7ebed2065962ad016a44eaee8bc7f97fdbf021e4
AL <alyssaliguori@Alyssas-MacBook-Air.local>
2018-09-18 19:18:01 -0400

    add object constructor and instantiate

---
+++ main.js
@@ -0,0 +1,41 @@
+//create an object constructor
+
+let Image = function(name, filePath, id){
+    this.name = name;
+    this.filePath = filePath;
+    this.id = id;
+}
+
+//instantiate new instances of Image constructor
+let Batman = new Image ('Batman','batman.jpg', 'batman')
+let Deadpool = new Image ('Deadpool', 'deadpool.jpg', 'deadpool')
+let Hulk = new Image ('Hulk', 'hulk.jpg', 'hulk')
+let Spiderman = new Image ('Spiderman', 'spiderman.jpg', 'spiderman')
+let Wolverine = new Image ('Wolverine', 'wolverine.jpg', 'wolverine')
+
+
+//instantiate new instances of object constructor for clothes
+
+let Jacket1 = new Image ('jacket', 'jacket_1.jpg', 'jacket')
+let Hat = new Image ('baseball cap', 'jacket_1.jpg', 'cap')
+let Shirt = new Image ('blue button-down', 'shirt_1.jpg','shirt' )
+let Shirt2 = new Image ('white button-down', 'shirt_2.jpg','shirt2')
+let Sneaker = new Image ('sneaker', 'shoe_1.jpg', 'sneaker')
+let Watch = new Image ('watch', 'watch_1.jpg', 'watch')
+let Shoe = new Image ('dress shoe', 'shoe_2.jpg', 'shoe')
+
+
+
+//instantiate new instances of object constructor for story paths
+romance_back.jpg
+tragedy_back.jpg
+comedy_back.jpg
+
+//instantiate new instances of object constructor for colors
+blue.gif
+orange.gif
+yellow.gif
+red.gif
+green.gif
+
+//instantiate new instances of object constructor for colors
