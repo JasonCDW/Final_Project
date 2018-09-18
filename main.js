03a9d191be4a04b5375f38fd168097a6a4ed8812
AL <alyssaliguori@Alyssas-MacBook-Air.local>
2018-09-18 19:23:03 -0400

    instantiating

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 //create an object constructor
 
-let Image = function(name, filePath, id){
+let Image = function(name, filePath, id, group){
     this.name = name;
     this.filePath = filePath;
     this.id = id;
-}
+    this.group = group;
+};
 
 //instantiate new instances of Image constructor
-let Batman = new Image ('Batman','batman.jpg', 'batman')
-let Deadpool = new Image ('Deadpool', 'deadpool.jpg', 'deadpool')
-let Hulk = new Image ('Hulk', 'hulk.jpg', 'hulk')
-let Spiderman = new Image ('Spiderman', 'spiderman.jpg', 'spiderman')
-let Wolverine = new Image ('Wolverine', 'wolverine.jpg', 'wolverine')
+let Batman = new Image ('Batman','batman.jpg', 'batman', 'superhero')
+let Deadpool = new Image ('Deadpool', 'deadpool.jpg', 'deadpool','superhero')
+let Hulk = new Image ('Hulk', 'hulk.jpg', 'hulk','superhero')
+let Spiderman = new Image ('Spiderman', 'spiderman.jpg', 'spiderman','superhero')
+let Wolverine = new Image ('Wolverine', 'wolverine.jpg', 'wolverine', 'superhero')
 
 
 //instantiate new instances of object constructor for clothes
 
-let Jacket1 = new Image ('jacket', 'jacket_1.jpg', 'jacket')
-let Hat = new Image ('baseball cap', 'jacket_1.jpg', 'cap')
-let Shirt = new Image ('blue button-down', 'shirt_1.jpg','shirt' )
-let Shirt2 = new Image ('white button-down', 'shirt_2.jpg','shirt2')
-let Sneaker = new Image ('sneaker', 'shoe_1.jpg', 'sneaker')
-let Watch = new Image ('watch', 'watch_1.jpg', 'watch')
-let Shoe = new Image ('dress shoe', 'shoe_2.jpg', 'shoe')
-
+let Jacket1 = new Image ('jacket', 'jacket_1.jpg', 'jacket','clothing')
+let Hat = new Image ('baseball cap', 'jacket_1.jpg', 'cap', 'clothing')
+let Shirt = new Image ('blue button-down', 'shirt_1.jpg','shirt','clothing')
+let Shirt2 = new Image ('white button-down', 'shirt_2.jpg','shirt2', 'clothing')
+let Sneaker = new Image ('sneaker', 'shoe_1.jpg', 'sneaker','clothing')
+let Watch = new Image ('watch', 'watch_1.jpg', 'watch', 'clothing')
+let Shoe = new Image ('dress shoe', 'shoe_2.jpg', 'shoe','clothing')
 
 
 //instantiate new instances of object constructor for story paths
-romance_back.jpg
-tragedy_back.jpg
-comedy_back.jpg
+let Romance = new Image ('romance', 'romance_back.jpg', 'romance','storyPath');
+let Tragedy = new Image ('tragedy','tragedy_back.jpg', 'tragedy','storyPath');
+let Comedy = new Image ('comedy', 'comedy_back.jpg', 'comedy', 'storyPath');
 
 //instantiate new instances of object constructor for colors
 blue.gif
