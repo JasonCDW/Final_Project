68f4e62257b6a86d016f177193c0b43491eedf42
JasonCDW <41653189+JasonCDW@users.noreply.github.com>
2018-09-22 11:01:42 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let elCreateStoryImg = document.getElementById('createStory');
+let elBody = document.getElementById('body');
+
+let bgimg;
+function changeBackgroundImage(obj) {
+    let bgimgsrc = obj.filePath;
+    bgimg = bgimgsrc;
+    console.log(elCreateStoryImg);
+    document.body.style.backgroundImage = 'url("'+bgimgsrc+'")';
+    console.log('test')
+    if (elCreateStoryImg) {
+        console.log('test 1')
+        elBody.removeChild(elCreateStoryImg);
+        console.log(elCreateStoryImg)
+        elCreateStoryImg = null;
+        console.log(elCreateStoryImg)
+    };
+}
