77df1c276f32043a95a5022a4b131d8060f926e2
JasonCDW <dw.jay.12@gmail.com>
2018-09-24 20:42:58 -0400

    borders added

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 // let elCreateStoryImg = document.getElementById('createStory');
 let elBody = document.getElementById('body');
 let j = 0
-
+let storyPath;
 let bgimg;
 function changeBackgroundImage(obj) {
+    if (obj.id === 'romance'){
+        storyPath = obj.id;
+        console.log(storyPath);
+    } else if (obj.id === 'comedy') {
+        storyPath = obj.id;
+        console.log(storyPath);
+    } else if (obj.id === 'tragedy') {
+        storyPath = obj.id;
+        console.log(storyPath);
+    };
     let bgimgsrc = obj.filePath;
     bgimg = bgimgsrc;
     // console.log(elCreateStoryImg);
