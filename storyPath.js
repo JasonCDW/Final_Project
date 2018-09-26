a24a1721bb1e2160b9febc920d868a92a6fb84e4
Greg Hsieh <greg.hsieh@gmail.com>
2018-09-26 19:03:49 -0400

    split story into 3 and added js

---
+++ storyPath.js
@@ -0,0 +1,15 @@
+let elCharText = document.getElementById('char-text');
+elCharText.innerHTML = 'JANE';
+
+let elSuperheroText = document.getElementById('superhero-text');
+elSuperheroText.innerHTML = 'SUPERHERO';
+
+let elClothText  = document.getElementById('clothes-text');
+elClothText.innerHTML = 'CLOTHING';
+
+let elColorSelect = document.getElementById('color-text');
+elColorSelect.innerHTML = 'COLOR';
+
+
+let elAnimalText = document.getElementById('animal-text');
+elAnimalText.innerHTML = 'ANIMAL';
